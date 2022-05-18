07bae5f83f6f862cea870c72749bf5ebf25fb479
Askhab <askhab.br@gmail.com>
2022-05-18 10:50:42 +0300

    soldier1

---
+++ objects.js
@@ -0,0 +1,18 @@
+const soldier = {
+name: 'tb',   
+health: 100,
+weapon = {
+nameWeapon: 'Barrett M82',
+ammo: 10,
+},
+supplies: 3,
+fire: function () {
+
+},
+reload: function () {
+
+},
+hurt: function () {
+
+},
+}
