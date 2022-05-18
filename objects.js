e05f8b54775130f7c4570c65494aa4585cd6c432
Askhab <askhab.br@gmail.com>
2022-05-18 11:49:10 +0300

    работа с методами

--- objects.js
+++ objects.js
@@ -1,18 +1,34 @@
 const soldier = {
 name: 'tb',   
-health: 100,
-weapon = {
-nameWeapon: 'Barrett M82',
-ammo: 10,
+health: 10,
+weapon: {
+nameWeapon: 'ak-74',
+ammo: 30,
 },
 supplies: 3,
 fire: function () {
-
+if (this.weapon.ammo !== 0) {
+this.weapon.ammo--
+console.log('бах-бах')
+} else if (this.weapon.ammo === 0) {
+console.log('обойма пуста. Перезаредитесь.')
+}
 },
 reload: function () {
-
+if (this.weapon.ammo === 0) {
+this.weapon.ammo += 30
+} else if (this.weapon.ammo === 30) {
+this.supplies--
+console.log('перезарядка...')   
+} else if (this.supplies === 0) {
+console.log('не осталось припасов')
+}
 },
 hurt: function () {
-
+if(this.health === 0 ){
+console.log('Ты умер')}
+else if (this.health--) {
+console.log('Ты ранен')
+}
 },
 }
